f8897fc2c0c3c8988260859eff30e6cb08caaaeb
imatheusaquino <matheus.etix@gmail.com>
2026-03-24 20:48:09 -0300

    ajuste estrutura site

--- simply-static-1-1774391846/wp-content/plugins/full-customer/app/assets/js/admin-login.js
+++
@@ -1,8 +0,0 @@
-jQuery(function ($) {
-  $("#enableChangeLoginUrl").on("change", function () {
-    $("#changedLoginUrl").parents("tr").toggleClass("hidden");
-    $("#changedLoginUrl")
-      .attr("required", this.checked)
-      .prop("required", this.checked);
-  });
-});
